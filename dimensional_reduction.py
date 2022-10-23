d8078a1642d920fe47ef4cb9b4351e9f2ee0afaf
Davicittod <davidhera96@gmail.com>
2022-10-23 13:54:15 +0200

    Update dimensional_reduction

--- dimensional_reduction.py
+++ dimensional_reduction.py
@@ -16,29 +16,28 @@ import itertools
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 
-import data_preprocess
 
 
 """
 Returns a dict with the resulting model of applying a OLS (Ordinary Least Square Regression) 
 and the RSS (Residual Sum of Squares) of the model.
 """
-def processSubset(X:pd.DataFrame, y:pd.Series, feature_set: List[str]):
-    model = sm.OLS(y,X[list(feature_set)])
+def processSubset(X_train:pd.DataFrame, y_train:pd.Series, X_test: pd.DataFrame, y_test:pd.Series, feature_set: List[str]):
+    model = sm.OLS(y_train, X_train[list(feature_set)])
     regr = model.fit()
-    RSS = ((regr.predict(X[list(feature_set)])-y)**2).sum()
+    RSS = ((regr.predict(X_test[list(feature_set)])-y_test)**2).sum()
     return {"model": regr, "RSS": RSS}
 
 """
 Evaluates the models adding one more feature
 Returns the best model of each iteration
 """
-def forward(X: pd.DataFrame, y:pd.Series, features: List[str] = []): 
-    remaining_features = [d for d in X.columns if d not in features]
+def forward(X_train: pd.DataFrame, y_train:pd.Series, X_test: pd.DataFrame, y_test:pd.Series, features: List[str] = []): 
+    remaining_features = [d for d in X_train.columns if d not in features]
     #stic = time.time()
     results=[]
     for d in remaining_features:
-        results.append(processSubset(X,y,features+[d]))
+        results.append(processSubset(X_train, y_train, X_test,y_test,features+[d]))
     models = pd.DataFrame(results)
     best_model = models.loc[models["RSS"].argmin()]
     #toc = time.time()
@@ -50,12 +49,12 @@ def forward(X: pd.DataFrame, y:pd.Series, features: List[str] = []):
 Evaluates the models removing one more feature
 Returns the best model of each iteration
 """
-def backward(X: pd.DataFrame, y:pd.Series, features: List[str] = []):
+def backward(X_train: pd.DataFrame, y_train:pd.Series, X_test: pd.DataFrame, y_test:pd.Series, features: List[str] = []):
     
     #tic = time.time()
     results = []
     for combo in itertools.combinations(features, len(features)-1):
-        results.append(processSubset(X,y,combo))
+        results.append(processSubset(X_train, y_train, X_test, y_test, combo))
     # Wrap everything up in a nice dataframe
     models = pd.DataFrame(results)
     # Choose the model with the highest RSS
@@ -68,31 +67,31 @@ def backward(X: pd.DataFrame, y:pd.Series, features: List[str] = []):
 """
 Defines the best set model according to the number of properties used (in the test set)
 """
-def getModels(X: pd.DataFrame, y:pd.Series, typeSelection: str = None):
+def getModels(X_train: pd.DataFrame, y_train:pd.Series, X_test: pd.DataFrame, y_test:pd.Series, typeSelection: str = None):
     
     if typeSelection == 'Backward':
-        models_bwd = pd.DataFrame(columns=["RSS", "model"], index = range(1,len(X.columns)))
+        print("Backward Test")
+        models_bwd = pd.DataFrame(columns=["RSS", "model"], index = range(1,len(X_train.columns)))
         tic = time.time()
-        features = X.columns
+        features = X_train.columns
         while(len(features) > 1):  
-            models_bwd.loc[len(features)-1] = backward(X, y, features)
+            models_bwd.loc[len(features)-1] = backward(X_train, y_train, X_test, y_test, features)
             features = models_bwd.loc[len(features)-1]["model"].model.exog_names
         toc = time.time()
         print("Total elapsed time:", (toc-tic), "seconds.")
         return models_bwd
 
     else: 
+        print("Forward Test")
         models_fwd = pd.DataFrame(columns=["RSS", "model"])
         tic = time.time()
         features = []
-        for i in range(1,len(X.columns)+1):
-            models_fwd.loc[i] = forward(X, y, features)
+        for i in range(1,len(X_train.columns)+1):
+            models_fwd.loc[i] = forward(X_train, y_train, X_test, y_test, features)
             features = models_fwd.loc[i]["model"].model.exog_names
         toc = time.time()
         print("Total elapsed time:", (toc-tic), "seconds.")
         return models_fwd
-
-    
 
 def plotErrors(rss, rsquared_adj, aic, bic):
     
@@ -133,18 +132,13 @@ def plotErrors(rss, rsquared_adj, aic, bic):
 
 """
 typeSelection accepts: 'Backward'|'Forward'
-preprocess in order to normalize and clean the data
 plot to show the graphicos of errors
 
 returns the best model for each error 
 """
-def dimensionalReduction(typeSelection: str ='Backward', preprocess: bool=True, plot: bool=False):
-    
-    X_train, X_test, y_train, y_test, column = data_preprocess.get_processed_train_test('./data/', preprocess)
-    col = column[column != 'Price']
-    X_train = pd.DataFrame(X_train, columns=col)
-    y_train = pd.Series(y_train)
-    models = getModels(X=X_train, y=y_train, typeSelection=typeSelection)
+def subsetSelection(X_train: pd.DataFrame, y_train:pd.Series, X_test: pd.DataFrame, y_test:pd.Series, typeSelection: str ='Backward', plot: bool=False):
+
+    models = getModels(X_train, y_train, X_test, y_test, typeSelection)
 
     #Type of error used
     errorEvaluated = ["RSS", "RsqAdj", "AIC", "BIC"]
@@ -170,4 +164,10 @@ def dimensionalReduction(typeSelection: str ='Backward', preprocess: bool=True, 
 
     bestModels = pd.DataFrame({"typeError": errorEvaluated, "model": chosenModels, "features": features})
     return bestModels
+
+def crossValidationApproach(typeSelection: str='Backeatd'):
+    pass
+
+
+
 
