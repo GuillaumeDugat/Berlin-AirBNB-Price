9cc4c29ee6da59bb6301c9a6bd9d16dcfd7f5763
Guillaume Dugat <dugatguillaume@gmail.com>
2022-10-25 16:50:22 +0200

    Simple imputing

--- data_preprocess.py
+++ data_preprocess.py
@@ -10,6 +10,7 @@ from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
 
 
 def convert_date(date_str: str) -> int:
@@ -21,7 +22,7 @@ def convert_date(date_str: str) -> int:
     return comparison
 
 
-class DropNan(BaseEstimator, TransformerMixin):
+class KeepColumns(BaseEstimator, TransformerMixin):
 
     def __init__(self, keep_columns):
         super().__init__()
@@ -34,8 +35,35 @@ class DropNan(BaseEstimator, TransformerMixin):
 
         X = X[self.keep_columns]
         X = X.replace('*', np.nan)
-        X = X.dropna()
 
+        return X
+
+class FeatureImputing(BaseEstimator, TransformerMixin):
+
+    def __init__(self, kwargs_dict):
+        super().__init__()
+        self.imputer_dict = {col: SimpleImputer(**kwargs_dict[col]) for col in kwargs_dict}
+    
+    def fit(self, X: pd.DataFrame, y=None):
+        for col in self.imputer_dict:
+            self.imputer_dict[col].fit(X[[col]])
+        return self
+
+    def transform(self, X: pd.DataFrame, y=None):
+        for col in self.imputer_dict:
+            X[col] = self.imputer_dict[col].transform(X[[col]])
+        return X
+
+class DropNan(BaseEstimator, TransformerMixin):
+
+    def __init__(self):
+        super().__init__()
+    
+    def fit(self, X: pd.DataFrame, y=None):
+        return self
+
+    def transform(self, X: pd.DataFrame, y=None):
+        X = X.dropna()
         return X
 
 class GetDummies(BaseEstimator, TransformerMixin):
@@ -63,7 +91,7 @@ class TransformDate(BaseEstimator, TransformerMixin):
     def transform(self, X: pd.DataFrame, y=None):
 
         for column in self.columns_dates:
-            X[column] = X[column].apply(convert_date)
+            X.loc[X[column].notnull(), column] = X.loc[X[column].notnull(), column].apply(convert_date)
 
         return X
 
@@ -134,6 +162,19 @@ def create_pipeline():
     'Longitude', 'Property Type', 'Room Type', 'Accomodates', 'Bathrooms', 'Bedrooms', 'Beds',
     'Guests Included', 'Min Nights', 'Reviews', 'Instant Bookable', 'Price']
 
+    imputing_dict = {
+        'Host Since': {'strategy': 'mean'},
+        'Is Superhost': {'strategy': 'most_frequent'},
+        'Property Type': {'strategy': 'most_frequent'},
+        'Room Type': {'strategy': 'most_frequent'},
+        'Accomodates': {'strategy': 'mean'},
+        'Bathrooms': {'strategy': 'mean'},
+        'Bedrooms': {'strategy': 'mean'},
+        'Beds': {'strategy': 'mean'},
+        'Guests Included': {'strategy': 'mean'},
+        'Min Nights': {'strategy': 'mean'},
+    }
+
     columns_to_encode = ['Neighborhood Group', 'Property Type', 'Room Type']
 
     columns_dates = ['Host Since']
@@ -144,9 +185,11 @@ def create_pipeline():
     columns_int = ['Accomodates', 'Bedrooms', 'Beds', 'Guests Included', 'Min Nights']
 
     pipeline_to_return = Pipeline(steps= [
-        ('dropnan', DropNan(columns_to_keep)),
-        ('onehot', GetDummies(columns_to_encode)),
+        ('keep_columns', KeepColumns(columns_to_keep)),
         ('dates', TransformDate(columns_dates)),
+        # ('imputing', FeatureImputing(imputing_dict)),
+        ('dropnan', DropNan()),
+        ('onehot', GetDummies(columns_to_encode)),
         ('boolean', TransformBoolean(columns_boolean)),
         ('location', TransformDistance(location_dict)),
         ('strings', TransformStrings(columns_float, columns_int))
@@ -189,7 +232,7 @@ def get_processed_train_test(path_to_folder: str='data/split_to_use', add_proces
     processing_pipeline = create_pipeline()
 
     train_processed_df = processing_pipeline.fit_transform(train_df)
-    test_processed_df = processing_pipeline.fit_transform(test_df)
+    test_processed_df = processing_pipeline.transform(test_df)
 
     columns = np.array(train_processed_df.columns)
 
@@ -201,8 +244,9 @@ def get_processed_train_test(path_to_folder: str='data/split_to_use', add_proces
 
 
     if add_processing:
-        X_train = StandardScaler().fit_transform(X_train)
-        X_test = StandardScaler().fit_transform(X_test)
+        scaler = StandardScaler()
+        X_train = scaler.fit_transform(X_train)
+        X_test = scaler.transform(X_test)
     else:
         X_train = X_train.to_numpy()
         X_test = X_test.to_numpy()
