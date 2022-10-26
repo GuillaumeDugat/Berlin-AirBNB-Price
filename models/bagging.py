d835c4c4490d03d4c58b83d2c862017f8360fab4
brillandthomas <brillandthomas@gmail.com>
2022-10-26 19:28:28 +0200

    corrections BEST_PARAMS

--- models/bagging.py
+++ models/bagging.py
@@ -5,12 +5,18 @@ from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.model_selection import RandomizedSearchCV, GridSearchCV
 
-def create_bagging_model():
+BEST_PARAMS_BAGGING = {}
+#MSE =
+
+def create_bagging_model(best_model: bool=False):
+
+    if best_model:
+        return BaggingRegressor(random_state=42, **BEST_PARAMS_BAGGING)
 
     regressor = BaggingRegressor(random_state=42)
     base_estimators = [LinearRegression(), DecisionTreeRegressor(random_state=42)]
 
-    param_grid = {
+    pgrid = {
         'base_estimator': base_estimators,
         'n_estimators': [int(x) for x in np.linspace(10,100, num=10)],
         'max_samples': [0.2,0.4,0.6,0.8,1.0],
@@ -19,8 +25,4 @@ def create_bagging_model():
         'boostrap_features': [True, False]
     }
 
-    grid_search = GridSearchCV(
-        estimator=regressor, param_grid=param_grid, scoring='neg_mean_squared_error', cv=3, verbose=2
-    )
-
-    return grid_search
+    return GridSearchCV(regressor, param_grid=pgrid, scoring='neg_mean_squared_error', cv=3, verbose=2)
--- models/svm.py
+++ models/svm.py
@@ -2,11 +2,17 @@ import numpy as np
 from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
 from sklearn.svm import SVR
 
-def create_SVR_model():
+BEST_PARAMS_SVR = {}
+#MSE = 
+
+def create_SVR_model(best_model: bool=False):
+
+    if best_model:
+        return SVR(**BEST_PARAMS_SVR)
 
     regressor = SVR()
 
-    parameters = {
+    pgrid = {
         'C': [10**k for k in range(-3, 3)],
         'tol': [10**(-4), 3*10**(-4), 10**(-3), 3*10**(-3), 10**(-2)],
         'epsilon': [0.01, 0.03, 0.1, 0.3, 1],
@@ -15,8 +21,4 @@ def create_SVR_model():
         'shrinking': [True,False]
     }
     
-    grid_search = GridSearchCV(
-        estimator=regressor, param_grid=parameters, scoring='neg_mean_squared_error', cv=3, verbose=2
-    )
-
-    return grid_search
+    return GridSearchCV(regressor, param_grid=pgrid, scoring='neg_mean_squared_error', cv=3, verbose=2)
