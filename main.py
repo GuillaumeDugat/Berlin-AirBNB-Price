aad7b41e60ad5bea03676d4896b13f437c53acfd
Guillaume Dugat <dugatguillaume@gmail.com>
2022-10-27 11:08:25 +0200

    Integrate PCA

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import numpy as np
-from sklearn.metrics import mean_squared_error, mean_absolute_error
 from sklearn.preprocessing import StandardScaler
 
 from preprocessing.baseline_preprocessing import get_baseline_preprocessed_train_test
+from preprocessing.pca import pcaSelection, plsSelection
 from models.baseline import create_mean_reg, create_KNN_mean_reg, create_linear_reg, create_random_predictor
 from models.boosting import create_ada_boost, create_gradient_boosted_tree, create_xg_boost
 from utils import print_eval
@@ -12,7 +11,9 @@ def preprocess(
     path_to_folder: str = 'data',
     remove_outliers: bool=True,
     imputing_missing_values: bool=False,
-    rescaling: bool = False
+    rescaling: bool = False,
+    pca: bool = False,
+    pls: bool = False,
 ):
     """Perform all the preprocessing tasks (baseline preprocessing, rescaling, pca or subset selection)"""
     # Baseline preprocessing
@@ -27,19 +28,35 @@ def preprocess(
         scaler = StandardScaler()
         X_train = scaler.fit_transform(X_train)
         X_test = scaler.transform(X_test)
+    
+    # Principal Component Analysis (with the best num_features, see preprocessing/pca.py)
+    if pca:
+        pca_transformer = pcaSelection()
+        X_train = pca_transformer.fit_transform(X_train)
+        X_test = pca_transformer.transform(X_test)
+
+    # Partial Least Squares (with the best n_components, see preprocessing/pca.py)
+    elif pls:
+        pls_transformer = plsSelection()
+        X_train, _ = pls_transformer.fit_transform(X_train, Y_train.ravel()) # TODO why does it return a tuple?
+        X_test = pls_transformer.transform(X_test)
 
     return X_train, X_test, Y_train, Y_test, columns
 
 
 def main():
     print('Preprocessing...\n')
+    ###############    Parameters should be modified here    ###############
     X_train, X_test, Y_train, Y_test, columns = preprocess(
         path_to_folder='data',
         remove_outliers=True,
         imputing_missing_values=False,
-        rescaling=False
+        rescaling=False,
+        pca=False,
+        pls=False,
     )
 
+    ###############    Model should be selected here    ###############
     # model = create_mean_reg()
     # model = create_KNN_mean_reg(columns)
     model = create_linear_reg()
--- preprocessing/pca.py
+++ preprocessing/pca.py
@@ -1,22 +1,20 @@
-import pandas as pd
 from sklearn.decomposition import PCA
 from sklearn.cross_decomposition import PLSRegression
 
+PCA_BEST_NUM_FEATURES = ...
+PLS_BEST_N_COMPONENTS = ...
 
-def pcaSelection(X_train: pd.DataFrame, numFeatures: int):
+
+def pcaSelection(numFeatures: int = PCA_BEST_NUM_FEATURES):
     """
     numFeatures is the number of components desired
     Returns the PCA model with the number of features desired
     """
-    pca_model = PCA(n_components=numFeatures)
-    X_new = pca_model.fit_transform(X_train)
-    return X_new
+    return PCA(n_components=numFeatures)
 
 
-def plsSelection(X_train: pd.DataFrame, y_train:pd.Series, n_components: int) :
+def plsSelection(n_components: int = PLS_BEST_N_COMPONENTS):
     """
     Returns the PLS Modified X_transform with the number of desired components
     """
-    pls = PLSRegression(n_components)
-    X_transform = pls.fit_transform(X_train, y_train.ravel())
-    return X_transform
+    return PLSRegression(n_components)
