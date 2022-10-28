5116b4a89d9d18215ccf869846acc7cd2432528b
Guillaume Dugat <dugatguillaume@gmail.com>
2022-10-28 17:28:21 +0200

    Modify parameters of boosting models

--- models/boosting.py
+++ models/boosting.py
@@ -1,5 +1,5 @@
 from sklearn.ensemble import AdaBoostRegressor, GradientBoostingRegressor 
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
 from sklearn.tree import DecisionTreeRegressor
 from xgboost import XGBRegressor
 from sklearn.preprocessing import LabelEncoder
@@ -7,6 +7,7 @@ from sklearn.preprocessing import LabelEncoder
 RANDOM_STATE = 42
 CROSS_VALIDATION = 5
 VERBOSE = 3
+N_ITER = 30
 
 BEST_PARAMS_ADA_BOOST = ...
 BEST_PARAMS_GRADIENT_BOOSTED_TREE = ...
@@ -26,32 +27,35 @@ def create_ada_boost(best_model: bool=False):
         return AdaBoostRegressor(random_state = RANDOM_STATE, **BEST_PARAMS_ADA_BOOST)
     ADBregr = AdaBoostRegressor(random_state = RANDOM_STATE)
     pgrid = {
-        'base_estimator': [DecisionTreeRegressor(random_state=RANDOM_STATE, max_depth=3), DecisionTreeRegressor(random_state=RANDOM_STATE, max_depth=5)],
-        'n_estimators': [4,8,15,30,50],
-        'learning_rate': [0.1], #[0.001, 0.005, 0.01, 0.05, 0.1],
-        'loss': ['linear'],
+        'base_estimator': [
+            DecisionTreeRegressor(random_state=RANDOM_STATE, max_depth=3),
+            DecisionTreeRegressor(random_state=RANDOM_STATE, max_depth=4),
+            DecisionTreeRegressor(random_state=RANDOM_STATE, max_depth=5)],
+        'n_estimators': [4, 8, 15, 30, 50, 80],
+        'learning_rate': [0.001, 0.01, 0.05, 0.1, 0.5, 1],
+        'loss': ['linear', 'square', 'exponential'],
     }
-    return GridSearchCV(ADBregr, param_grid=pgrid, scoring='neg_mean_squared_error', cv=CROSS_VALIDATION, verbose=VERBOSE)
+    return RandomizedSearchCV(ADBregr, param_distributions=pgrid, scoring='neg_mean_squared_error', n_iter=N_ITER, cv=CROSS_VALIDATION, verbose=VERBOSE)
 
 def create_gradient_boosted_tree(best_model: bool=False):
     if best_model:
         return GradientBoostingRegressor(random_state = RANDOM_STATE, **BEST_PARAMS_GRADIENT_BOOSTED_TREE)
     GDBreg  = GradientBoostingRegressor(random_state = RANDOM_STATE)
     pgrid = {
-        'n_estimators': [4,8,15,30,50, 100],
-        'learning_rate': [0.1], #[0.001, 0.01, 0.05, 0.1, 0.5, 1],
-        'max_depth': [1, 3, 5],
-        'loss': ['squared_error'], #['squared_error', 'absolute_error', 'huber', 'quantile'],
+        'n_estimators': [4, 8, 15, 30, 50, 80],
+        'learning_rate': [0.001, 0.01, 0.05, 0.1, 0.5, 1],
+        'max_depth': [2, 3, 5],
+        'loss': ['squared_error', 'absolute_error', 'huber', 'quantile'],
     }
-    return GridSearchCV(GDBreg, param_grid=pgrid, scoring='neg_mean_squared_error', cv=CROSS_VALIDATION, verbose=VERBOSE)
+    return RandomizedSearchCV(GDBreg, param_distributions=pgrid, scoring='neg_mean_squared_error', n_iter=N_ITER, cv=CROSS_VALIDATION, verbose=VERBOSE)
 
 def create_xg_boost(best_model: bool=False):
     if best_model:
         return XGBRegressorCustom(random_state = RANDOM_STATE, **BEST_PARAMS_XG_BOOST)
     ADBregr  = XGBRegressorCustom(random_state = RANDOM_STATE)
     pgrid = {
-        'n_estimators': [5, 10, 50, 100], #[500, 750, 1000],
-        'learning_rate': [0.1], #[0.01, 0.05, 0.1],
-        'max_depth': [2, 3],
+        'n_estimators': [4, 8, 15, 30, 50, 80, 100, 250, 500],
+        'learning_rate': [0.001, 0.01, 0.05, 0.1, 0.5, 1],
+        'max_depth': [2, 3, 5],
     }
-    return GridSearchCV(ADBregr, param_grid=pgrid, scoring='neg_mean_squared_error', cv=CROSS_VALIDATION, verbose=VERBOSE)
+    return RandomizedSearchCV(ADBregr, param_distributions=pgrid, scoring='neg_mean_squared_error', n_iter=N_ITER, cv=CROSS_VALIDATION, verbose=VERBOSE)
